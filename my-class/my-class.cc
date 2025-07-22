198f4818c396033993cedf557ec0dbf77e6b7c6e
richard <gent.rich.yin@gmail.com>
2025-07-22 02:19:42 +0000

    modified my-class.cc

--- my-class/my-class.cc
+++ my-class/my-class.cc
@@ -7,13 +7,28 @@ public:
     A() {
         cout << "Class A default constructor called" << endl;
     }
+    // Copy constructor (deep copy)
+    A(const A& other) {
+        cout << "Class A copy constructor called" << endl;
+    }
     ~A() {
         cout << "Class A deconstructor called" << endl;
     }
 };
 
+void f1(A a) {
+    cout << "Inside f1(A a)" << endl;    
+}
+
+void f2(A& a) {
+    cout << "Inside f2(A& a)" << endl;    
+}
+
 int main() {
     A a;
     A* b = new A();
     delete b;
+    A c = a;
+    f1(c);
+    f2(c);
 }
